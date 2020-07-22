3fade6f3a12b35d55262e7cd46c6efd4bc7b9a7d
dcpiturlea <dcpiturlea@gmail.com>
2020-07-22 19:49:28 +0300

    v2

--- PulseSensorSketch/PulseSensorSketch.ino
+++ PulseSensorSketch/PulseSensorSketch.ino
@@ -1,7 +1,7 @@
 
-
+  
 //  Variables
-int PulseSensorPurplePin = 0;        // Senzorul de puls este conectat la pinul o
+int PulseSensorPurplePin = A0;        // Senzorul de puls este conectat la pinul o
 int LED13 = 13;   // Ledul care va licari este legat la punul 13
 
 
@@ -17,7 +17,7 @@ int firstTime=0;  //Variabila care masoara timpul de la pornirea aplicatiei
 int minBPM =45;   //puls minim
 int maxBPM = 200; //puls maxim
 boolean wasfirstPulse = false; //variabila care devine true la prima bataie de inima
-
+int pulseInterval = 0;
 // Setarile programului(ruleaza o singura data)
 void setup() {
   pinMode(LED13,OUTPUT);         // pinul va aprinde si va stinge ledul la fiecare bataie a inimii(pin 13)
@@ -30,6 +30,7 @@ void setup() {
 
 // "Functie" care se repeta la infinit
 void loop() {
+  noTone(8);
   if (countBP >0){
     wasfirstPulse ==true;
   }
@@ -37,8 +38,8 @@ void loop() {
   Signal = analogRead(PulseSensorPurplePin);  // Citim valoarea in mod analogic si o asignam variabilei signal.
 
     
-   if (ThisTime - lastTimeFromCalculationBpm>=10000 && wasBPM == true  && minBPM > 44 && maxBPM <201){
-        BPM = (countBP *6);
+  if (ThisTime - lastTimeFromCalculationBpm>=15000 && wasBPM == true  && minBPM > 44 && maxBPM <201){
+        BPM = (countBP *4);
         lastTimeFromCalculationBpm = millis();
         countBP = 0;
         Serial.println("BPM: " + String(BPM));  
@@ -48,18 +49,20 @@ void loop() {
    if(Signal > Threshold+50 && wasBPM == false){ //Cand senzorul analog primeste un semnal peste 600 se aduna o bataie, daca wasBPM este true
     countBP=countBP+1;
     digitalWrite(LED13,HIGH);
+    tone(8, 960);
     wasBPM = true;
-    Serial.println("Batai inima: " + String(countBP));
+   // Serial.println("Batai inima: " + String(countBP));
   
    // BPM=ThisTime-LastTime;
     //BPM=int(60/(float(BPM)/1000));
    // Serial.println("BPM: " + String(BPM));
     
-   }
+   }  
    if(Signal < Threshold-50 && wasBPM == true){  //daca semnal scade sub 500 si bataie este falsa, aceasta devine adevarata
     digitalWrite(LED13,LOW);
     wasBPM = false;
     LastTime=millis();
+    noTone(8);
 
    }
 
